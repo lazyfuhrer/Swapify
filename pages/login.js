ffdd63d7ec241aaba8bb10dc0768853251e2c988
Biswarghya Biswas <iambiswarghya@gmail.com>
2022-11-27 21:03:00 +0530

    wallets

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,11 @@
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet';
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 import { signIn } from 'next-auth/react';
 import { useAccount, useConnect, useSignMessage, useDisconnect } from 'wagmi';
 import { useRouter } from 'next/router';
 import axios from 'axios';
+import { Button } from '@chakra-ui/react';
 
 function SignIn() {
     const { connectAsync } = useConnect();
@@ -11,14 +14,32 @@ function SignIn() {
     const { signMessageAsync } = useSignMessage();
     const { push } = useRouter();
 
-    const handleAuth = async () => {
+    const handleAuth = async (wallet) => {
         if (isConnected) {
             await disconnectAsync();
         }
 
-        const { account, chain } = await connectAsync({ connector: new MetaMaskConnector() });
+        const userData = { network: 'evm' };
 
-        const userData = { address: account, chain: chain.id, network: 'evm' };
+        if (wallet === 'metamask') {
+            const { account, chain } = await connectAsync({ connector: new MetaMaskConnector() });
+            userData.address = account;
+            userData.chain = chain.id;
+        }
+
+        if (wallet === 'walletConnect') {
+            const { account, chain } = await connectAsync({ connector: new WalletConnectConnector({options: {qrcode:true}}) });
+            userData.address = account;
+            userData.chain = chain.id;
+        }
+
+        if (wallet === 'coinbase') {
+            const { account, chain } = await connectAsync({ connector: new CoinbaseWalletConnector() });
+            userData.address = account;
+            userData.chain = chain.id;
+        }
+
+        // const userData = { address: account, chain: chain.id, network: 'evm' };
 
         const { data } = await axios.post('/api/auth/request-message', userData, {
             headers: {
@@ -42,7 +63,12 @@ function SignIn() {
     return (
         <div>
             <h3>Web3 Authentication</h3>
-            <button onClick={() => handleAuth()}>Authenticate via Metamask</button>
+            <Button onClick={() => handleAuth("metamask")} bgColor={'teal.300'}>Authenticate via Metamask</Button>
+            <br/>
+            <Button onClick={() => handleAuth("walletConnect")} bgColor={'teal.500'}>Authenticate via WalletConnect</Button>
+            <br/>
+            <Button onClick={() => handleAuth("coinbase")} bgColor={'teal.700'}>Authenticate via Coinbase</Button>
+
         </div>
     );
 }
