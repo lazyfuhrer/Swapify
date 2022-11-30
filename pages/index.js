58d8c71ae21b12c3205dd190bf7ec9f6453cb74d
Biswarghya Biswas <iambiswarghya@gmail.com>
2022-11-30 23:56:26 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import { Box, Button, Flex, Heading } from '@chakra-ui/react'
+import { Box, Button, Flex, Heading, Link } from '@chakra-ui/react'
 import axios from 'axios'
+import NextLink from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
@@ -9,9 +10,12 @@ export default function Home() {
   return (
     <div>
       <Box align={'center'}>
-        <Heading py={'150'} px={'50'}>
+        <Heading mt={'150'} px='50' mb={'75'}>
           Welcome to Swapify - Converts Native tokens to ERC20
         </Heading>
+        <Link as={NextLink} href='/login' fontSize={'2xl'}>
+          Connect your wallet
+        </Link>
       </Box>
     </div>
   )
